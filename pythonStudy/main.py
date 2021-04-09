f94965cf14729b5972bcd60882cefb900305f8e4
Kimdaeki97 <daeki56@gmail.com>
2021-04-09 23:21:29 +0900

    "04.09"

--- pythonStudy/main.py
+++ pythonStudy/main.py
@@ -7,3 +7,10 @@ def my_function():
 my_function()
 print(x)
 
+# 상수는 일반 변수와 구분 짓기 위해 대문자로 표기한다.
+# 실수를 방지하기 위해서이기도 함
+# 상수는 절대 바꾸지 않겠다는 의지(?)를 표현
+
+PI = 3 # 상수
+def calculate_area(r):
+    return PI * r * r
