18b424b9c3c81693117253fd36f2e9b158303bae
Kimdaeki97 <daeki56@gmail.com>
2021-04-10 00:08:46 +0900

    "04.09"

--- pythonStudy/main.py
+++ pythonStudy/main.py
@@ -1,16 +1,36 @@
-x = 2 # 글로벌 변수
+# x = 2 # 글로벌 변수
+#
+# def my_function():
+#     x = 3  # 로컬 변수
+#     print(x)
+#
+# my_function()
+# print(x)
+#
+# # 상수는 일반 변수와 구분 짓기 위해 대문자로 표기한다.
+# # 실수를 방지하기 위해서이기도 함
+# # 상수는 절대 바꾸지 않겠다는 의지(?)를 표현
+#
+# PI = 3 # 상수
+# def calculate_area(r):
+#     return PI * r * r
 
-def my_function():
-    x = 3  # 로컬 변수
-    print(x)
+# 04.09 - 거스름돈 계산 프로그램
 
-my_function()
-print(x)
+def calculate_change(payment, cost):
+    left = payment - cost
 
-# 상수는 일반 변수와 구분 짓기 위해 대문자로 표기한다.
-# 실수를 방지하기 위해서이기도 함
-# 상수는 절대 바꾸지 않겠다는 의지(?)를 표현
+    fifty = left // 50000
+    ten = (left % 50000) // 10000
+    five = (left % 10000) // 5000
+    one = (left % 5000) // 1000
 
-PI = 3 # 상수
-def calculate_area(r):
-    return PI * r * r
+    print("50000원 지폐: {}장".format(fifty))
+    print("10000원 지폐: {}장".format(ten))
+    print("5000원 지폐: {}장".format(five))
+    print("1000원 지폐: {}장".format(one))
+
+
+calculate_change(100000, 33000)
+print()
+calculate_change(500000, 378000)
