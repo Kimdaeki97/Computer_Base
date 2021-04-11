bc1a090c966cfae5dd027c63ad6e9262ba4554f9
Kimdaeki97 <daeki56@gmail.com>
2021-04-11 22:15:54 +0900

    "04.11"

--- pythonStudy/main.py
+++ pythonStudy/main.py
@@ -49,3 +49,50 @@ print(i)
 #     print(i * 2)
 #     i += 1
 
+# 04.11
+temperature = 11
+if temperature <= 10:
+    print("자켓을 입는다.")
+else:
+    print("자켓을 입지 않는다.")
+
+# 학점 계산기
+def print_grade(midterm_score, final_score):
+    total = midterm_score + final_score
+
+    if total >= 90:
+        print("A")
+    elif 90 > total >= 80:
+        print("B")
+    elif 80 > total >= 70:
+        print("C")
+    elif 70 > total >= 60:
+        print("D")
+    else:
+        print("F")
+
+print_grade(40, 45)
+print_grade(20, 35)
+print_grade(30, 32)
+print_grade(50, 45)
+
+# 100이하 자연수 중 8의 배수이지만, 12의 배수는 아닌 것은
+i = 1
+while i <= 100:
+    if (i % 8 == 0) and (i % 12 != 0) :
+        print(i)
+    i += 1
+
+# 1,000보다 작은 자연수 중 2 또는 3의 배수의 합은?
+i = 1
+total = 0
+
+while i < 1000:
+    if (i % 2 == 0) or (i % 3 == 0):
+        total += i
+    i += 1
+
+print(total)
+
+
+
