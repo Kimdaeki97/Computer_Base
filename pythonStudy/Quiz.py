8622b1d9b5ae53c3a16a3e93b7805b1b4ab7807a
Kimdaeki97 <daeki56@gmail.com>
2021-04-16 10:06:30 +0900

    "04.16"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -152,6 +152,18 @@ print(numbers)
 numbers.sort()
 print(numbers)
 
+# 04.16
+numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+# 인덱스와 원소 출력
+for i in range(len(numbers)):
+    print(i, numbers[i])
+
+
+
+
+
+
 
 
 
