23284920b1ba23df2d221b60539b77c9049939e6
Kimdaeki97 <daeki56@gmail.com>
2021-04-16 10:12:29 +0900

    "04.16"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -159,6 +159,10 @@ numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 for i in range(len(numbers)):
     print(i, numbers[i])
 
+# 2의 제곱 출력
+for i in range(11):
+    print("{}^{} = {}".format(2, i, 2 ** i))
+
 
 
 
