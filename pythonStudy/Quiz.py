fbf1984cfda9e170298eadabe5722ffa99bee462
Kimdaeki97 <daeki56@gmail.com>
2021-04-20 22:50:04 +0900

    "04.19"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -183,3 +183,18 @@ for a in range(1, 1000):
 
 print("-------------------------------")
 
+# 04. 20
+
+# for문을 이용해서 리스트 뒤집기
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+# 리스트 뒤집기
+for left in range(len(numbers) // 2):
+    # 인덱스 left와 대칭인 인덱스 right 계산
+    right = len(numbers) - left - 1
+
+    # 위치 바꾸기
+    numbers[right], numbers[left] = numbers[left], numbers[right]
+
+print("뒤집어진 리스트: " + str(numbers))
+
