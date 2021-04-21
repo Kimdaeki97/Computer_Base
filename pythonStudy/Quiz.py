a03fae729c392095eeb51ef0cdc07e5ed6e1890d
Kimdaeki97 <daeki56@gmail.com>
2021-04-22 00:39:20 +0900

    "04.22"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -216,3 +216,32 @@ vocab["privilege"] = "특권"
 vocab["principle"] = "원칙"
 print(vocab)
 
+print("-------------------------------")
+
+# 언어 사전의 단어와 뜻을 서로 바꿔주는 함수
+def reverse_dict(dict):
+    new_dict = {}  # 새로운 사전
+
+    # dict의 key와 value를 뒤집어서 new_dict에 저장
+    for key, value in dict.items():
+        new_dict[value] = key
+
+    return new_dict  # 변환한 새로운 사전 리턴
+
+# 영-한 단어장
+vocab = {
+    'sanitizer': '살균제',
+    'ambition': '야망',
+    'conscience': '양심',
+    'civilization': '문명',
+    'privilege': '특권',
+    'principles': '원칙'
+}
+
+# 기존 단어장 출력
+print("영-한 단어장\n{}\n".format(vocab))
+
+# 변환된 단어장 출력
+reversed_vocab = reverse_dict(vocab)
+print("한-영 단어장\n{}".format(reversed_vocab))
+
