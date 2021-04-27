8832e138a790e31a12a4fa7bf114ef01b0861a13
Kimdaeki97 <daeki56@gmail.com>
2021-04-27 15:42:39 +0900

    "04.27"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -309,7 +309,7 @@ print("-------------------------------")
 
 def mask_security_number(security_number):
     # 코드를 입력하세요.
-
+    return security_number[0:-4] + str('****')
 
 # 테스트
 print(mask_security_number("880720-1234567"))
