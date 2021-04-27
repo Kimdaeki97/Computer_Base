9b68fe158ff7f771becf213639975bcbcdc90a29
Kimdaeki97 <daeki56@gmail.com>
2021-04-27 14:27:47 +0900

    "04.27"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -288,6 +288,19 @@ print(y)
 
 print("-------------------------------")
 
+# 04.27
+# 자리수 합 리턴
+def sum_digit(num):
+    total = 0
+    str_num = str(num)
+
+    for i in str_num:
+        total += int(i)
+    return total
+
+# sum_digit(1)부터 sum_digit(1000)까지의 합 구하기
+# 코드를 입력하세요.
+
 
 
 
