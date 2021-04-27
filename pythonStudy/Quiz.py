a56e3d8f55ed247b20fd9d4e8c4d91ad8b5e246e
Kimdaeki97 <daeki56@gmail.com>
2021-04-27 15:08:44 +0900

    "04.27"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -299,6 +299,26 @@ def sum_digit(num):
     return total
 
 # sum_digit(1)부터 sum_digit(1000)까지의 합 구하기
+sum = 0
+for num in range(0,1001):
+    sum = sum + sum_digit(num)
+
+print(sum)
+
+print("-------------------------------")
+
+def mask_security_number(security_number):
+    # 코드를 입력하세요.
+
+
+# 테스트
+print(mask_security_number("880720-1234567"))
+print(mask_security_number("8807201234567"))
+print(mask_security_number("930124-7654321"))
+print(mask_security_number("9301247654321"))
+print(mask_security_number("761214-2357111"))
+print(mask_security_number("7612142357111"))
+
 
 
 
