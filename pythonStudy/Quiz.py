c15c463784713183a53619ca1953961f71426144
Kimdaeki97 <daeki56@gmail.com>
2021-04-28 23:31:11 +0900

    "04.28"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -323,10 +323,14 @@ print("-------------------------------")
 
 # 04.28
 def is_palindrome(word):
-    # 코드를 입력하세요.
-    i = 0
-    for i in len[word]:
-        word[i] == word[len(word) - i - 1]
+    for left in range(len(word) // 2):
+        # 한 쌍이라도 일치하지 않으면 바로 False를 리턴하고 함수를 끝냄
+        right = len(word) - left -1
+        if word[left] != word[right]:
+            return False
+
+    # for문에서 나왔다면 모든 상이 일치
+    return True
 
 
 # 테스트
