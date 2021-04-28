4cefd10d4de4ad785e88ac3ba0eeef5cee7cdd9c
Kimdaeki97 <daeki56@gmail.com>
2021-04-28 23:35:22 +0900

    "04.28"

--- pythonStudy/Quiz.py
+++ pythonStudy/Quiz.py
@@ -324,12 +324,11 @@ print("-------------------------------")
 # 04.28
 def is_palindrome(word):
     for left in range(len(word) // 2):
-        # 한 쌍이라도 일치하지 않으면 바로 False를 리턴하고 함수를 끝냄
-        right = len(word) - left -1
-        if word[left] != word[right]:
+        # 한 쌍이라도 일치하지 않으면 바로 False를 리턴하고 함수를 끝낸다.
+        right = len(word) - 1 - left
+        if word[right] != word[left]:
             return False
-
-    # for문에서 나왔다면 모든 상이 일치
+    # for문에서 나왔다면 모든 쌍이 일치
     return True
 
 
