e0ccd16e0139df09d64befd7787acb8c0337e028
Kimdaeki97 <daeki56@gmail.com>
2021-05-04 15:15:08 +0900

    "05.04"

--- pythonStudy/Quiz_2.py
+++ pythonStudy/Quiz_2.py
@@ -3,11 +3,24 @@ import random
 ANSWER = random.randint(1, 20)
 NUM_TRIES = 4
 
-guess = int(input("기회가 {}번 남았습니다. 1-20 사이의 숫자를 맞혀 보세요.".format(NUM_TRIES)))
+guess = -1
+tries = 0
+"""
+게임이 계속되어야하는 조건 두가지
+1. 아직 사용자가 정답을 맞히지 못했다. = guess != ANSWER
+2. 아직 사용자에게 기회가 남았다. = tries가 4가 되지 않았다.
+"""
+while guess != ANSWER and tries < NUM_TRIES:
+    guess = int(input("기회가 {}번 남았습니다. 1-20 사이의 숫자를 맞혀 보세요.".format(NUM_TRIES - tries)))
+    print(guess)
+    tries += 1
 
+    if guess < ANSWER:
+        print("UP")
+    elif guess > ANSWER:
+        print("DOWN")
 
-if tryy == ANSWER:
-    print("축하합니다. {}번만에 숫자를 맞히셨습니다.".format(NUM_TRIES))
+if guess == ANSWER:
+    print("축하합니다 {}번 만에 숫자를 맞히셨습니다.".format(tries))
 else:
-    NUM_TRIES - 1
-    print(tryy)
+    print("아쉽습니다. 정답은 {}였습니다.".format(ANSWER))
