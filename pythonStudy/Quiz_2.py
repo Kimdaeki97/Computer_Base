4751709da62e9ca1721939f0afb1393bb4067ecd
Kimdaeki97 <daeki56@gmail.com>
2021-05-04 14:46:44 +0900

    "05.04"

--- pythonStudy/Quiz_2.py
+++ pythonStudy/Quiz_2.py
@@ -1,1 +1,13 @@
-quiz_2
+# 05.04
+import random
+ANSWER = random.randint(1, 20)
+NUM_TRIES = 4
+
+guess = int(input("기회가 {}번 남았습니다. 1-20 사이의 숫자를 맞혀 보세요.".format(NUM_TRIES)))
+
+
+if tryy == ANSWER:
+    print("축하합니다. {}번만에 숫자를 맞히셨습니다.".format(NUM_TRIES))
+else:
+    NUM_TRIES - 1
+    print(tryy)
