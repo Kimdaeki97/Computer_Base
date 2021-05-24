a13edb7c9bf4aaaa7cebc36e629e63451edf1c28
Kimdaeki97 <daeki56@gmail.com>
2021-05-24 22:52:27 +0900

    "05.24"

--- pythonStudy/baseball/baseball.py
+++ pythonStudy/baseball/baseball.py
@@ -1,6 +1,17 @@
+import random
 from random import randint
 
-def generate_numbers:
+def generate_numbers():
     numbers = []
+
+    while len(numbers) < 3:
+        num = randint(0, 9)
+        if num not in numbers:
+            numbers.append(num)
+
+    print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
+    return numbers
+
+print(generate_numbers())
 
 
