ddb7e61cbbf0ee7f2e4443dcc51222ca6d27d75a
Kimdaeki97 <daeki56@gmail.com>
2021-06-11 22:39:24 +0900

    "06.11"

--- pythonStudy/baseball/baseball.py
+++ pythonStudy/baseball/baseball.py
@@ -11,3 +11,15 @@ def generate_numbers():
 
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
+
+def generate_number():
+    numbers = []
+
+    while len(numbers) < 3:
+        num = randint(0. 9)
+        if num not in numbers:
+            numbers.append(num)
+
+    print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
+    return numbers
+
